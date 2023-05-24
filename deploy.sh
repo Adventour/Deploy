282b0c23b8fb400c745eedccbe4fb7c02a2d5122
김민석 <76719828+pxxnxx@users.noreply.github.com>
2023-05-24 21:30:25 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,18 @@
-#!/bin/bash
+# !/bin/bash
 
 # Installing docker engine if not exists
-# if ! type docker > /dev/null
-# then
-#   echo "docker does not exist"
-#   echo "Start installing docker"
-#   sudo apt-get update
+if ! type docker > /dev/null
+then
+  echo "docker does not exist"
+  echo "Start installing docker"
+  sudo apt-get update
 #   sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 #   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 #   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 #   sudo apt update
 #   apt-cache policy docker-ce
 #   sudo apt install -y docker-ce
-# fi
+fi
 
 # Installing docker-compose if not exists
 # if ! type docker-compose > /dev/null
