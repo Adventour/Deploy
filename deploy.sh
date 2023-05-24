eb744ac9eba5cb5b0bc2880d942ddd2a729e5c75
김민석 <76719828+pxxnxx@users.noreply.github.com>
2023-05-24 21:35:52 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -15,13 +15,13 @@ then
 fi
 
 # Installing docker-compose if not exists
-# if ! type docker-compose > /dev/null
-# then
-#   echo "docker-compose does not exist"a
-#   echo "Start installing docker-compose"
-#   sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-#   sudo chmod +x /usr/local/bin/docker-compose
-# fi
+if ! type docker-compose > /dev/null
+then
+  echo "docker-compose does not exist"a
+  echo "Start installing docker-compose"
+#  sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#  sudo chmod +x /usr/local/bin/docker-compose
+fi
 
 # # aws log setting
 #   sudo systemctl daemon-reload
